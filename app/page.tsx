f32ec61a59c1beb601063c142f5df06fd93292da
timgamerx <iyanuoluwa3822@gmail.com>
2026-09-04 20:23:11 +0100

    update latest changes

--- app/page.tsx
+++ app/page.tsx
@@ -163,9 +163,9 @@ export default function Home() {
 
       <Link
         href="/createpost"
-        className="fixed bottom-6 right-6 z-50 flex items-center justify-center bg-gray-100 text-white p-4 rounded-full cursor-pointer shadow-lg transition-colors"
+        className="fixed bottom-6 right-6 z-50 flex items-center justify-center bg-gray-200 text-white p-4 rounded-full cursor-pointer shadow-lg hover:bg-gray-300 transition-colors"
       >
-        {/* <Send size={18} className="text-gray-100" hover:bg-gray-300 /> */}
+        <Send size={18} className="text-gray-100" />
       </Link>
     </div>
   );
